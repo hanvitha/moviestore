8386e1072a978708a34afe41b906a86e035a6026
Hanvitha Gavini <gavini.honey@gmail.com>
2019-11-05 17:48:00 -0600

    changing worker to gevent

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
-workers = int(os.environ.get('GUNICORN_PROCESSES', '5'))
-threads = int(os.environ.get('GUNICORN_THREADS', '3'))
-timeout = 60000
+NUM_WORKERS=4
+NUM_THREADS=4
+keepalive = 100
+worker_class = 'gevent'
+# workers = int(os.environ.get('GUNICORN_PROCESSES', NUM_WORKERS))
+# threads = int(os.environ.get('GUNICORN_THREADS', NUM_THREADS))
+timeout = 120
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+loglevel = 'debug'
