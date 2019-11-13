4343a1b7da78365afaed9369d1b2eafd376324b3
Hanvitha Gavini <hxg163330@utdallas.edu>
2019-11-13 11:06:12 -0600

    back to gunicorn

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-# import os
-# NUM_WORKERS=4
-# NUM_THREADS=4
+import os
+NUM_WORKERS=2
+NUM_THREADS=4
 keepalive = 100
-worker_class = 'gevent'
-# workers = int(os.environ.get('GUNICORN_PROCESSES', NUM_WORKERS))
-# threads = int(os.environ.get('GUNICORN_THREADS', NUM_THREADS))
-workers = 2
-threads = 4
+# worker_class = 'gevent'
+workers = int(os.environ.get('GUNICORN_PROCESSES', NUM_WORKERS))
+threads = int(os.environ.get('GUNICORN_THREADS', NUM_THREADS))
+# workers = 2
+# threads = 4
 timeout = 120
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
